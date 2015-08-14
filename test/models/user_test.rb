57531cecc4432552a5543ab07f7af7e7cb09a292
SpencerHorn <spencer.horn@smashingboxes.com>
2015-08-14 14:01:36 -0400

    Add user following plus CH 12 exercises

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,4 +80,33 @@ class UserTest < ActiveSupport::TestCase
             @user.destroy
         end
     end
+
+    test "should follow and unfollow a user" do
+        chester = users(:chester)
+        archer  = users(:archer)
+        assert_not chester.following?(archer)
+        chester.follow(archer)
+        assert chester.following?(archer)
+        assert archer.followers.include?(chester)
+        chester.unfollow(archer)
+        assert_not chester.following?(archer)
+    end
+
+    test "feed should have the right posts" do
+        chester  = users(:chester)
+        archer   = users(:archer)
+        lana     = users(:lana)
+        # Posts from followed user
+        lana.microposts.each do |post_following|
+            assert chester.feed.include?(post_following)
+        end
+        # Post from self
+        chester.microposts.each do |post_self|
+            assert chester.feed.include?(post_self)
+        end
+        # Posts from unfollowed user
+        archer.microposts.each do |post_unfollowed|
+            assert_not chester.feed.include?(post_unfollowed)
+        end
+    end
 end
